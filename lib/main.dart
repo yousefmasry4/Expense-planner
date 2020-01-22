ca686de2864295a5273f5845fdb1b0a2ff9b0f3d
youssef <yuoefmasry4@gmail.com>
2020-01-22 21:37:38 +0200

    main init

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,74 @@
 import 'package:flutter/material.dart';
-void main(){
+import 'package:untitled/transaction.dart';
+
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
-@override
-  Widget build(BuildContext context) {
-  var questions=["what's your favorite color? ",
-                  "what's your favorite animal"];
-  // TODO: implement build
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("hello world"),),
-      body: Column(
-        children: <Widget>[
-          Text(questions[0]),
-          RaisedButton(child:Text("yellow"),onPressed: null,),
-          RaisedButton(child:Text("blue"),onPressed: null,),
-          RaisedButton(child:Text("red"),onPressed: null,),
-          RaisedButton(child:Text("black"),onPressed: null,),
 
-        ],
+class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "new shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(id: "t2", title: "pizza", amount: 39.99, date: DateTime.now()),
+    Transaction(
+        id: "t3", title: "new shoes", amount: 55.99, date: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("hello world"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Text(
+                    '\$'+tx.amount.toString(),
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.green,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.green,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                    ),
+                    Text(
+                      tx.date.toUtc().toString(),
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(169, 169, 169, 80)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        }).toList(),
       ),
     ));
   }
-
 }
